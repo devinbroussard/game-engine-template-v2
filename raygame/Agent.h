dce720e79d9ac9bdc6d069b9efc74c0f7c2e8b62
Devin <devin.ray.broussard@gmail.com>
2022-02-23 16:59:25 -0600

    Filled out part of the agent header

--- raygame/Agent.h
+++ raygame/Agent.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "Actor.h"
+#include "DynamicArray.h"
+class MoveComponent;
+class SteeringBehavior;
+
 class Agent :
 	public Actor {
 public:
@@ -8,9 +12,20 @@ public:
 
 	//Returns the target variable
 	Actor* getTarget() { return m_target; } 
+	float getForce();
+	void setForce(float force);
+
+	void start() override;
+	void update(float deltaTime) override;
+	void onAddComponent(Component* component);
 
 private:
+	DynamicArray<SteeringBehavior*> m_steeringBehaviors;
 	//An actor that the agent will seek or flee from
 	Actor* m_target;
+	float m_maxForce;
+	float m_maxVelocity;
+	MoveComponent* m_moveComponent;
+	float m_force;
 };
 
